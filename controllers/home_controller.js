2d90b38ea8304e719aef36ddc06f59b6e98c6069
vaibhavkharbanda <sahil031@gmail.com>
2022-02-22 14:00:12 +0530

    commiting update with ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 module.exports.home = function(req,res){
-    return res.end('<h1> Express is up for ToDOList!</h1>');
+    return res.render('home', {
+        title:"ToDoList By Vaibhav"
+    });
 }
-
